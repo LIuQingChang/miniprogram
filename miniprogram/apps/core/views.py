64357f48b397639d6e995b458dcf55e07791285d
liuqingchang <neoliu1204@gmail.com>
2020-05-13 15:34:44 +0800

    feat: init project

--- miniprogram/apps/core/views.py
+++ miniprogram/apps/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import hashids
 
-# Create your views here.
+salt = "test"
+
+i = hashids.Hashids(salt="sasasa", min_length=8)  # 添加一个salt ，Hashide通过接收salt值支持对散列的散列，可确保唯一性
+ensecret = i.encode(12345678)
+print(ensecret)
+desecret = i.decode(ensecret+'123')
+print(desecret)
--- miniprogram/wsgi.py
+++ miniprogram/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miniprogram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miniprogram.settings.base')
 
 application = get_wsgi_application()
