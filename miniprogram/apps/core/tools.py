0a5ed479308ae188ae7834df4f66989f85348c88
liuqingchang <neoliu1204@gmail.com>
2020-05-21 15:11:36 +0800

    feat: add check params sendsms

--- miniprogram/apps/core/tools.py
+++ miniprogram/apps/core/tools.py
@@ -1,13 +1,34 @@
-from functools import wraps
-import datetime
-
 import hashids
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.request import RpcRequest
 
 
-class ParamsProcess:
+def check_params(
+        *args,
+        partial=True,
+        **kwargs
+):
+    try:
+        params_set = args[0]
+    except IndexError:
+        raise IndexError("请输入参数列表")
 
-    def __init__(self):
-        self.hash_func = hashids.Hashids(salt="test", min_length=8)
+    if partial:
+        if set(kwargs.keys()).issubset(params_set):
+            return True
+        else:
+            raise Exception("参数未通过检查")
+    else:
+        if params_set == kwargs.keys():
+            return True
+        else:
+            raise Exception("参数未通过检查")
+
+
+class Params2Code:
+
+    def __init__(self, salt):
+        self.hash_func = hashids.Hashids(salt=salt, min_length=4)
 
     def __encode_id(self, common_id):
         return self.hash_func.encode(common_id)
@@ -28,7 +49,7 @@ class ParamsProcess:
                 else:
                     kwargs['params'] = self.__decode_id(encode_id)
         except IndexError:
-            raise IndexError("Need parse key list")
+            raise IndexError("请输入要解密的key列表")
         return kwargs
 
     def auto_encode(self, *args, **kwargs):
@@ -44,30 +65,29 @@ class ParamsProcess:
                 else:
                     kwargs['params'] = self.__encode_id(decode_id)
         except IndexError:
-            raise IndexError("Need parse key list")
+            raise IndexError("请输入要解密的key列表")
         return kwargs
 
 
-class TimeRelation:
+class SendSMS:
+    REGION = "cn-hangzhou"
+    PRODUCT_NAME = "Dysmsapi"
+    DOMAIN = "dysmsapi.aliyuncs.com"
 
-    @staticmethod
-    def get_today(time_status):
-        if isinstance(time_status, datetime.time):
-            return datetime.datetime.combine(datetime.datetime.today(), time_status)
-        if time_status == 'midnight':
-            return datetime.datetime.combine(datetime.datetime.today(), datetime.time.min)
-        elif time_status == 'over':
-            return datetime.datetime.combine(datetime.datetime.today(), datetime.time.max)
-        else:
-            return None
+    def __init__(self, access_key_id, access_key_secret):
+        self.acs_client = AcsClient(access_key_id, access_key_secret, self.REGION)
 
-    @staticmethod
-    def get_someday(abs_num, time_status):
-        if isinstance(time_status, datetime.time):
-            return datetime.datetime.combine(datetime.datetime.today() + datetime.timedelta(abs_num), time_status)
-        if time_status == 'midnight':
-            return datetime.datetime.combine(datetime.date.today() + datetime.timedelta(abs_num), datetime.time.min)
-        elif time_status == 'over':
-            return datetime.datetime.combine(datetime.date.today() + datetime.timedelta(abs_num), datetime.time.min)
-        else:
-            return None
+    def send_sms(self, template_param=None, **kwargs):
+        check_params({"OutId", "PhoneNumbers", "SignName", "TemplateCode"}, partial=False, **kwargs)
+        sms_request = RpcRequest(self.PRODUCT_NAME, '2017-05-25', 'SendSms')
+
+        sms_request.__params = {}
+        sms_request.__params.update(**kwargs)
+
+        if template_param is not None:
+            sms_request.__params.update({'TemplateParam': template_param})
+        sms_request.set_query_params(sms_request.__params)
+
+        sms_response = self.acs_client.do_action_with_exception(sms_request)
+
+        return sms_response
